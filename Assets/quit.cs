ef260dfde4cf10e42eb8b3caf211d1104fafa73a
Htet Myat Tun <129264079+thi28t@users.noreply.github.com>
2024-10-24 14:20:08 +0700

    UI system

---
+++ Assets/quit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public void Quitgame()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    
+    }
+}
